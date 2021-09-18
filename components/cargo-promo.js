45787f769486c145c651612bccf88b2342d2aea7
Adam J. Arling <aarling@gmail.com>
2021-09-18 08:39:50 -0500

    Cargo pre-sale link

--- components/cargo-promo.js
+++ components/cargo-promo.js
@@ -2,6 +2,7 @@ import Section from "components/section";
 import Image from "next/image";
 import ExternalLink from "components/external-link";
 import FandTS from "components/f-and-ts";
+import LinkButton from "./link-button";
 
 export default function CargoPromo() {
   return (
@@ -9,24 +10,33 @@ export default function CargoPromo() {
       <Section>
         <h3 className="pb-0">Full-length Album October 15, 2021</h3>
         <h3 className="text-gray-600">Europe, UK, Japan</h3>
-        <div className="relative h-24 md:h-36 w-36 md:w-48 mb-4 float-left mr-4">
-          <ExternalLink url="https://www.cargorecords.co.uk/">
-            <Image
-              src="/images/cargo-records.jpeg"
-              alt="Cargo Records"
-              layout="fill"
-              objectFit="cover"
-            />
-          </ExternalLink>
+        <div className="pb-12">
+          <div className="relative h-24 md:h-36 w-36 md:w-48 mb-4 float-left mr-4">
+            <ExternalLink url="https://www.cargorecords.co.uk/">
+              <Image
+                src="/images/cargo-records.jpeg"
+                alt="Cargo Records"
+                layout="fill"
+                objectFit="cover"
+              />
+            </ExternalLink>
+          </div>
+          <p className="text-md md:text-xl">
+            <ExternalLink url="https://www.cargorecords.co.uk/">
+              Cargo Records UK
+            </ExternalLink>{" "}
+            will be distributing <FandTS /> full album of singles (including 2
+            unreleased tracks) on Vinyl and CD in UK/Europe/Japan. Release date:{" "}
+            <strong>OCTOBER 15, 2021</strong>
+          </p>
         </div>
-        <p className="text-md md:text-xl">
-          <ExternalLink url="https://www.cargorecords.co.uk/">
-            Cargo Records UK
-          </ExternalLink>{" "}
-          will be distributing <FandTS /> full album of singles (including 2
-          unreleased tracks) on Vinyl and CD in UK/Europe/Japan. Release date:{" "}
-          <strong>OCTOBER 15, 2021</strong> Pre-order coming soon...
-        </p>
+
+        <LinkButton
+          isInverted
+          href="https://cargorecordsdirect.co.uk/products/furbaby-the-tight-spaces-furbaby-the-tight-spaces"
+        >
+          Pre-Order the Album
+        </LinkButton>
       </Section>
     </div>
   );
--- components/link-button.js
+++ components/link-button.js
@@ -1,7 +1,13 @@
 import Link from "next/link";
 
-export default function LinkButton({ children, href }) {
-  return (
+export default function LinkButton({ children, href, isInverted }) {
+  return isInverted ? (
+    <Link href={href}>
+      <a className="bg-black hover:bg-gray-800 text-gray-100 font-sans uppercase hover:text-gray-200 py-2 px-8 tracking-widest border-none rounded text-xl">
+        {children}
+      </a>
+    </Link>
+  ) : (
     <Link href={href}>
       <a className="bg-transparent hover:bg-gray-200 text-gray-100 font-sans uppercase hover:text-gray-900 py-2 px-8 border border-solid border-gray-200 hover:border-transparent tracking-widest rounded text-xl">
         {children}
