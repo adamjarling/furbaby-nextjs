37d0285ce490dfb1ada922e30911957fb8e56a64
Adam J. Arling <aarling@gmail.com>
2022-08-13 10:10:39 -0500

    update header

--- components/header.js
+++ components/header.js
@@ -27,6 +27,13 @@ const navLinks = [
   },
 ];
 
+const navigation = [
+  { name: "Music", href: "/music" },
+  { name: "Photos", href: "/photos" },
+  { name: "Press", href: "/press" },
+  { name: "Tour", href: "/shows" },
+];
+
 export default function Header() {
   const [active, setActive] = React.useState(false);
   const [isNavBg, setIsNavBg] = React.useState(false);
@@ -36,112 +43,33 @@ export default function Header() {
   });
 
   return (
-    <>
-      <header
-        className={classNames(
-          [
-            "fixed",
-            "top-0",
-            "z-20",
-            "min-w-full",
-            "transition",
-            "duration-1000",
-          ],
-          {
-            "bg-black": isNavBg,
-            "bg-opacity-70": isNavBg,
-            "bg-opacity-0": !isNavBg,
-          }
-        )}
-      >
-        <div className="flex justify-between items-center px-4 md:px-8">
-          {/* Desktop nav */}
-          <nav className="hidden lg:block text-sm uppercase">
-            {navLinks.map(({ url, label }) => (
-              <span key={url} className="px-10">
-                <Link href={url}>
-                  <a className="border-none">{label}</a>
-                </Link>
-              </span>
-            ))}
-          </nav>
-
-          {/* Hamburger menu mobile nav */}
-          <div
-            className={names(
-              ["tham", "tham-e-squeeze", "tham-w-8", "lg:hidden"],
-              {
-                "tham-active": active,
-              }
-            )}
-            onClick={() => setActive(!active)}
-          >
-            <div className="tham-box">
-              <div className="tham-inner bg-gray-100" />
-            </div>
-          </div>
-
-          <Link href="/">
-            <a className="relative border-none w-40 pt-1">
-              <Image
-                src={logo}
-                alt="Furbaby and the Tight Spaces"
-                layout="responsive"
-                width={1640}
-                height={1077}
+    <header className="bg-gradient-to-b from-black to-transparent fixed z-10 w-full">
+      <nav className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8" aria-label="Top">
+        <div className="w-full py-6 flex items-center justify-between ">
+          <div className="flex items-center">
+            <a href="/" className="border-0">
+              <span className="sr-only">Workflow</span>
+              <img
+                className="h-10 md:h-20 w-auto"
+                src="/images/furbaby-and-ts-logo_white.png"
+                alt=""
               />
             </a>
-          </Link>
-
-          <div className="invisible w-0 h-0 md:visible md:w-auto md:h-auto text-2xl">
-            <SocialMediaIcons />
+            <div className="hidden ml-10 space-x-8 lg:block"></div>
+          </div>
+          <div className="ml-10 space-x-4">
+            {navigation.map((link) => (
+              <a
+                key={link.name}
+                href={link.href}
+                className="uppercase text-white hover:text-indigo-50 border-0"
+              >
+                {link.name}
+              </a>
+            ))}
           </div>
         </div>
-      </header>
-      <nav
-        className={names(
-          [
-            "z-10",
-            "fixed",
-            "transition-all",
-            "duration-1000",
-            "ease-in-out",
-            "h-screen",
-            "w-screen",
-            "bg-black",
-            "text-3xl",
-            "sm:text-5xl",
-            "md:text-6xl",
-            "font-display",
-            "text-center",
-            "tracking-widest",
-            "h-0",
-          ],
-          {
-            "opacity-90": active,
-            "opacity-0": !active,
-            "h-full": active,
-            visible: active,
-            invisible: !active,
-          }
-        )}
-      >
-        <div className="flex flex-col items-center justify-center h-full">
-          <ul className="">
-            {navLinks.map((nl) => (
-              <li key={nl.label} className="py-5 sm:py-8 font-sans uppercase">
-                <Link href={nl.url}>
-                  <a className="border-none">{nl.label}</a>
-                </Link>
-              </li>
-            ))}
-          </ul>
-        </div>
-
-        <div className="flex items-center justify-center w-full text-2xl mb-12 absolute bottom-0">
-          <SocialMediaIcons />
-        </div>
       </nav>
-    </>
+    </header>
   );
 }
