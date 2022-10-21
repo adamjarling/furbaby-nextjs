831b4b5362dffd29008c55d3249061bebba6df2f
Adam J. Arling <aarling@gmail.com>
2022-10-21 15:12:03 -0500

    Suppress posts

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,64 +1,64 @@
 import Layout from "components/layout";
-import { getAllPostIds, getPostData } from "lib/posts";
-import Head from "next/head";
-import Date from "components/date";
-import Image from "next/image";
-import Section from "components/section";
-import { getSortedPostsData } from "lib/posts";
-import Link from "next/link";
-import ExternalLink from "components/external-link";
-import { ParallaxBanner, Parallax } from "react-scroll-parallax";
-import ParallaxBannerChildren from "components/parallax-banner-children";
-import LinkButton from "components/link-button";
+// import { getAllPostIds, getPostData } from "lib/posts";
+// import Head from "next/head";
+// import Date from "components/date";
+// import Image from "next/image";
+// import Section from "components/section";
+// import { getSortedPostsData } from "lib/posts";
+// import Link from "next/link";
+// import ExternalLink from "components/external-link";
+// import { ParallaxBanner, Parallax } from "react-scroll-parallax";
+// import ParallaxBannerChildren from "components/parallax-banner-children";
+// import LinkButton from "components/link-button";
 
-export async function getStaticProps({ params }) {
-  const postData = await getPostData(params.id);
-  const allPostsData = getSortedPostsData();
+// export async function getStaticProps({ params }) {
+//   const postData = await getPostData(params.id);
+//   const allPostsData = getSortedPostsData();
 
-  return {
-    props: {
-      postData,
-      allPostsData,
-    },
-  };
-}
+//   return {
+//     props: {
+//       postData,
+//       allPostsData,
+//     },
+//   };
+// }
 
-export async function getStaticPaths() {
-  const paths = getAllPostIds();
-  return {
-    paths,
-    fallback: false,
-  };
-}
+// export async function getStaticPaths() {
+//   const paths = getAllPostIds();
+//   return {
+//     paths,
+//     fallback: false,
+//   };
+// }
 
 export default function Post({ postData, allPostsData }) {
-  const currentIndex = allPostsData.findIndex(
-    (item) => item.id === postData.id
-  );
+  // const currentIndex = allPostsData.findIndex(
+  //   (item) => item.id === postData.id
+  // );
 
-  const renderPrev = () => {
-    const prevIndex =
-      currentIndex === 0 ? allPostsData.length - 1 : currentIndex - 1;
-    const prev = allPostsData[prevIndex];
+  // const renderPrev = () => {
+  //   const prevIndex =
+  //     currentIndex === 0 ? allPostsData.length - 1 : currentIndex - 1;
+  //   const prev = allPostsData[prevIndex];
 
-    // if (prev.externalUrl)
-    //   return <ExternalLink url={prev.externalUrl}>{prev.title}</ExternalLink>;
-    return <Link href={`/posts/${prev.id}`}>{prev.title}</Link>;
-  };
+  //   // if (prev.externalUrl)
+  //   //   return <ExternalLink url={prev.externalUrl}>{prev.title}</ExternalLink>;
+  //   return <Link href={`/posts/${prev.id}`}>{prev.title}</Link>;
+  // };
 
-  const renderNext = () => {
-    const nextIndex =
-      currentIndex === allPostsData.length - 1 ? 0 : currentIndex + 1;
-    const next = allPostsData[nextIndex];
+  // const renderNext = () => {
+  //   const nextIndex =
+  //     currentIndex === allPostsData.length - 1 ? 0 : currentIndex + 1;
+  //   const next = allPostsData[nextIndex];
 
-    // if (next.externalUrl)
-    //   return <ExternalLink url={next.externalUrl}>{next.title}</ExternalLink>;
-    return <Link href={`/posts/${next.id}`}>{next.title}</Link>;
-  };
+  //   // if (next.externalUrl)
+  //   //   return <ExternalLink url={next.externalUrl}>{next.title}</ExternalLink>;
+  //   return <Link href={`/posts/${next.id}`}>{next.title}</Link>;
+  // };
 
   return (
     <Layout>
-      <Head>{postData.title}</Head>
+      {/* <Head>{postData.title}</Head>
       <div className="relative pt-32">
         <Image
           src={`/images/posts/${postData.coverImage}`}
@@ -93,7 +93,7 @@ export default function Post({ postData, allPostsData }) {
       <nav className="px-6 py-6 md:px-12 md:py-12 flex justify-between uppercase md:text-xl w-full">
         <div className="max-w-sm">{renderPrev()}</div>
         <div className="max-w-sm text-right">{renderNext()}</div>
-      </nav>
+      </nav> */}
     </Layout>
   );
 }
